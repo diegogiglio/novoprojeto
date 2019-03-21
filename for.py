5e530547a68466e700de7644c00e87c041240d4e
diegogiglio <48689961+diegogiglio@users.noreply.github.com>
2019-03-20 21:33:54 -0300

    tabuada

---
+++ for.py
@@ -0,0 +1,3 @@
+vezes = 5
+for n in range(1, vezes + 1):
+    print(f"imprimindo {n}")
---
+++ tabuada for.py
@@ -0,0 +1,7 @@
+#calcula tabuada
+numero = int(input("Informe a tabuada que deseja calcular "))
+numerovezes = int(input("Informe a quantidade de multiplicações "))
+for n in range(1, numerovezes + 1):
+    resultado = n * numero
+    print(f"{n} * {numero} = {resultado}")
+
